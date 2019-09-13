3bcda3a94734a19619128d7ce9254a15bf8362c3
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2019-09-12 22:29:01 -0300

    PUT User

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,8 +22,46 @@ class UserController {
 	}
 
 	async update(req, res) {
-		console.log(req.userId);
-		return res.json({ ok: true });
+		const { name, email } = req.body;
+
+		const user = await User.findByPk(req.userId);
+
+		if (email !== user.email) {
+			const userExists = await User.findOne({
+				where: { email },
+			});
+
+			if (userExists) {
+				return res.status(400).json({
+					error: {
+						type: 'DuplicateEmail',
+						message: `There is already a user with email '${email}'.`,
+					},
+				});
+			}
+		}
+
+		const { id, provider } = await user.update({ name, email });
+
+		return res.json({ id, name, email, provider });
+	}
+
+	async updatePassword(req, res) {
+		// const { oldPassword, password } = req.body;
+		// if (oldPassword) {
+		// }
+		// if (password) {
+		// }
+		// const user = await User.findByPk(req.userId);
+		// if (!(await user.checkPassword(oldPassword))) {
+		// 	return res.status(401).json({
+		// 		error: {
+		// 			type: 'PasswordDoesNotMatch',
+		// 			message: 'The password does not match your old password.',
+		// 		},
+		// 	});
+		// }
+		// return res.json({ ok: true });
 	}
 }
 
